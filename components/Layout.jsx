53bc7dedaecd270c1eff8e20a5709d798332060e
Kelvin Tobar <71950186+ktobar@users.noreply.github.com>
2023-02-06 18:04:41 -0500

    changed header

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Kelvin's Store</title>
+        <title>Moni Luv Dance</title>
       </Head>
       <header>
         <Navbar />
